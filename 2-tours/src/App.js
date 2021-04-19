1a746156c5613a7d5ca5285dc5239224a58aacc9
dctien <dctien00@gmail.com>
2021-04-19 14:58:51 +0700

    done tour

--- 2-tours/src/App.js
+++ 2-tours/src/App.js
@@ -4,8 +4,53 @@ import Tours from './Tours'
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tours-project'
+
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const [loading, setLoading] = useState(true)
+  const [tours, setTours] = useState([])
+
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour)=>tour.id !== id)
+    setTours(newTours)
+  }
+
+  const fetchTours = async () => {
+    setLoading(true)
+    try {
+      const respone = await fetch(url)
+      const tours = await respone.json()
+      setLoading(false)
+      setTours(tours)
+    } catch (error) {
+      setLoading(false)
+      console.log(error)
+    }
+  }
+  useEffect(() => {
+    fetchTours()
+  }, [])
+  if(loading){
+    return (
+      <main>
+        <Loading/>
+      </main>
+    );
+  }
+  
+  if (tours.length === 0){
+    return <main>
+      <div className="title">
+        <h2>No tour left</h2>
+        <button className="btn" onClick={fetchTours}>Refesh</button>
+      </div>
+    </main>
+  }
+
+  return (
+    <main>
+      <Tours tours={tours} removeTour={removeTour} />
+    </main>
+  )
 }
 
 export default App
